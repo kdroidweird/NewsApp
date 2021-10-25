b2a334a0cba730d5a2dc91e19302c056e747a73c
Balazs Nobik <balazsnobik@gmail.com>
2021-10-25 16:24:26 +0200

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "9620ba06cf3e43078e6383be759ae87d"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
